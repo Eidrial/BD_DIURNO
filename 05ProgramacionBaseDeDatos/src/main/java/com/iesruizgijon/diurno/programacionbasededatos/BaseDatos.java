c2db6e46e9b27669c7925a03d9d3ee6ba3ca2995
“Eidrial” <azaharaparrales@gmail.com>
2024-02-22 10:59:26 +0100

    actualizacion

--- 05ProgramacionBaseDeDatos/src/main/java/com/iesruizgijon/diurno/programacionbasededatos/BaseDatos.java
+++ 05ProgramacionBaseDeDatos/src/main/java/com/iesruizgijon/diurno/programacionbasededatos/BaseDatos.java
@@ -6,7 +6,10 @@ package com.iesruizgijon.diurno.programacionbasededatos;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,15 +22,15 @@ public class BaseDatos {
     //Atributos
     private Connection conexion;
 
-    private String URL = "jdbc:mysql://localhost:3306/";
-    private String user;
-    private String password;
-    private String nameDB;
+    private final String USER;
+    private final String PASS;
+    private final String nameDB;
+    private final String URL = "jdbc:mysql://127.0.0.1:3306/";
 
     //Constructor
-    public BaseDatos(String user, String password, String nameDB) {
-        this.user = user;
-        this.password = password;
+    public BaseDatos(String nameDB, String USER, String PASS) {
+        this.USER = USER;
+        this.PASS = PASS;
         this.nameDB = nameDB;
     }
 
@@ -35,13 +38,11 @@ public class BaseDatos {
     //Para conectar
     public void conecta() {
 
-        Connection conexion;
-
         try {
-            conexion = DriverManager.getConnection(URL + nameDB, user, password);
+            conexion = DriverManager.getConnection(URL + nameDB, USER, PASS);
             System.out.println("Conexión establecida...");
         } catch (SQLException ex) {
-            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("Error de conexión.");
         }
     }
@@ -60,4 +61,74 @@ public class BaseDatos {
         }
     }
 
+    public void consultaPrueba() {
+        String company;
+        String apellidos;
+        String nombre;
+
+        try {
+            Statement sentencia = conexion.createStatement();
+            ResultSet resultado = sentencia.executeQuery("select company, last_name, first_name from customers limit 10");
+            while (resultado.next()) {
+                company = resultado.getString("company");
+                apellidos = resultado.getString("last_name");
+                nombre = resultado.getString("first_name");
+
+                System.out.println(" Company " + company + " Apellidos " + apellidos + " Nombre " + nombre);
+            }
+            resultado.close();
+            sentencia.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(BaseDatos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void consultaPrueba2() {
+        String nombre;
+        int identificadorPedido;
+
+        try {
+            Statement sentencia = conexion.createStatement();
+            ResultSet resultado = sentencia.executeQuery("select concat(last_name, ' ' ,first_name) as nombre, orders.id as identificadorPedido from customers INNER JOIN orders ON customers.id = orders.customer_id limit 10;");
+
+            while (resultado.next()) {
+                nombre = resultado.getString("nombre");
+                identificadorPedido = resultado.getInt("identificadorPedido");
+
+                System.out.println(" Nombre del cliente: " + nombre + " \n Identificador del pedido: " + identificadorPedido + "\n");
+            }
+            resultado.close();
+            sentencia.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(BaseDatos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public String[] describe(String nombre) {
+
+        String[] columnas = null;
+        int n_columnas;
+        int i;
+
+        try {
+
+            Statement statement = conexion.createStatement();
+            ResultSet resultset = statement.executeQuery("SELECT * FROM " + nombre);
+            ResultSetMetaData metadatos = resultset.getMetaData();
+
+            n_columnas = metadatos.getColumnCount();
+            columnas = new String[n_columnas];
+
+            for (i = 1; i <= n_columnas; i++) {
+                columnas[i - 1] = metadatos.getColumnName(i);
+            }
+        } catch (SQLException ex) {
+
+            Logger.getLogger(BaseDatos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return columnas;
+    }
 }
---
+++ 05ProgramacionBaseDeDatos/src/main/java/com/iesruizgijon/diurno/programacionbasededatos/Principal.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.iesruizgijon.diurno.programacionbasededatos;
+
+/**
+ *
+ * @author azahara
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        final String USER = "root";
+        final String PASS = "123qweASD_";
+        final String nameDB = "northwind";
+
+        BaseDatos bd = new BaseDatos(nameDB, USER, PASS);
+
+        bd.conecta();
+
+        System.out.println("\nPRUEBA 1 ~ CONSULTA SIMPLE:\n");
+        bd.consultaPrueba();
+
+        System.out.println("\n\nPRUEBA 2 ~ CONSULTA CONCATENAR APELLIDOS-NOMBRE CON LOS PEDIDOS REALIZADOS:\n");
+        bd.consultaPrueba2();
+
+        System.out.println("\n\nPRUEBA 3 ~ IMPLEMENTA LA FUNCIÓN 'DESCRIBE' DE UN CLIENTE MYSQL:\n");
+        String[] TablaPedidos = bd.describe("orders");
+
+        for (String TablaPedido : TablaPedidos) {
+            System.out.println(" " + TablaPedido);
+        }
+        System.out.println("");
+
+        bd.desconecta();
+
+    }
+
+}
